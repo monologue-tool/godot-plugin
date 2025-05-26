9eed9be5e4bf542ae1a1db2d0e7d5b1547f4b20c
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-05-26 11:15:42 +0200

    Remove useless functions

--- addons/monologue/core/monologue_context.gd
+++ addons/monologue/core/monologue_context.gd
@@ -8,7 +8,6 @@ var nodes: Array :
 	get: return timeline.node_list
 var next_node_id: String = ""
 
-var characters_pool: Array
 var options: Dictionary :
 	get: return timeline.options
 var variables: Dictionary :
@@ -26,14 +25,6 @@ func get_next_node() -> Dictionary:
 func display_text(text: String, speaker_name: String = "", override_speed: int = settings.text_speed) -> void:
 	timeline.text_box.display(text, speaker_name, override_speed)
 	await timeline.text_box.display_finished
-	
-
-func display_option() -> void:
-	pass
-
-
-func clear_all_options() -> void:
-	pass
 
 
 func get_node_from_id(node_id: String) -> Dictionary: return timeline.get_node_from_id(node_id)
